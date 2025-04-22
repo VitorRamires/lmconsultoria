b9fa37bc9964fdb669aa4698cd80f5ed43ba12d5
Vitor Ramires <vmr.rs@hotmail.com>
2025-04-22 18:28:01 -0300

    response home-page 4

--- src/components/homePage/tecnologias.tsx
+++ src/components/homePage/tecnologias.tsx
@@ -7,27 +7,28 @@ import SalesSrc from "@/assets/images/salesforce.png";
 export function Technologies() {
   return (
     <>
-      <section className="technologies"></section>
+      <section className="technologies">
         <div className="center">
-        <div className="tech-title">Tecnologias</div>
-        <div className="tech-box">
-          <div className="tech">
-            <img src={AuSrc} alt="" />
-          </div>
-          <div className="tech">
-            <img src={SapSrc} alt="" />
-          </div>
-          <div className="tech">
-            <img src={ExcelSrc} alt="" />
-          </div>
-          <div className="tech">
-            <img src={UiSrc} alt="" />
-          </div>
-          <div className="tech">
-            <img src={SalesSrc} alt="" />
+          <div className="tech-title">Tecnologias</div>
+          <div className="tech-box">
+            <div className="tech">
+              <img src={AuSrc} alt="" />
+            </div>
+            <div className="tech">
+              <img src={SapSrc} alt="" />
+            </div>
+            <div className="tech">
+              <img src={ExcelSrc} alt="" />
+            </div>
+            <div className="tech">
+              <img src={UiSrc} alt="" />
+            </div>
+            <div className="tech">
+              <img src={SalesSrc} alt="" />
+            </div>
           </div>
         </div>
-        </div>
+      </section>
     </>
   );
 }
