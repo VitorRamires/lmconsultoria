f7ba72ffbd0d385e19ee6a84b8f86fb28b7c0eb3
Vitor Ramires <vmr.rs@hotmail.com>
2025-04-24 11:57:20 -0300

    header mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,30 @@
 import { NavLink } from "react-router-dom";
 import BrasilFlag from "../assets/images/brasil.png";
 import EuaFlag from "../assets/images/estados-unidos.png";
+import { useState } from "react";
 
 export function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  function handleMenuBehavior() {
+    setOpenMenu(!openMenu);
+  }
+
   return (
     <>
+      <div
+        className={`btn-button-header ${openMenu ? "show" : "hidden"}`}
+        onClick={handleMenuBehavior}
+      >
+        <div className="center">
+          <div className="btn-menu">
+            <div className="line line1"></div>
+            <div className="line line2"></div>
+            <div className="line line3"></div>
+          </div>
+        </div>
+      </div>
+
       <div className="header">
         <div className="header-extras">
           <div className="L">
